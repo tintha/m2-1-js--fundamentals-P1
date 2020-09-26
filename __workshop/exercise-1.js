c001394abb464a7caf85a82b61ea0ec3b52dd878
Thira Inthavongsa <tintha@gmail.com>
2020-09-26 19:41:59 -0400

    Finished ex 1

--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -8,12 +8,12 @@ function question1() {
 
   console.log("1.1. ( )"); /* "I am a "Horse""    */
   console.log("1.2. ( )"); /* "I 'prefer' ducks'  */
-  console.log("1.3. ( )"); /* 'Yes, duck is nice' */
-  console.log("1.4. ( )"); /* "Ah, but I\'m vegan!" */
+  console.log("1.3. (x)"); /* 'Yes, duck is nice' */
+  console.log("1.4. (x)"); /* "Ah, but I\'m vegan!" */
   console.log("1.5. ( )"); /* 'You'll eat salad then' */
-  console.log("1.6. ( )"); /* 'Yes I\'ll eat salad' */
-  console.log("1.7. ( )"); /* "I'm happy to hear that!" */
-  console.log("1.8. ( )"); /* "\"Happy to hear " + 'that" ' + "he says!" */
+  console.log("1.6. (x)"); /* 'Yes I\'ll eat salad' */
+  console.log("1.7. (x)"); /* "I'm happy to hear that!" */
+  console.log("1.8. (x)"); /* "\"Happy to hear " + 'that" ' + "he says!" */
   console.log("1.9. ( )"); /* “Hello world!” */
 }
 
@@ -23,10 +23,10 @@ function question2() {
   // Add an 'x' between the ( ) to indicate that it's true.
 
   console.log("2.1. ( )"); /* 7 == 2    */
-  console.log("2.2. ( )"); /* 7 == 7    */
-  console.log("2.3. ( )"); /* 7 == '7'  */
-  console.log("2.4. ( )"); /* 7 != 0    */
-  console.log("2.5. ( )"); /* 7 !== '7' */
+  console.log("2.2. (x)"); /* 7 == 7    */
+  console.log("2.3. (x)"); /* 7 == '7'  */
+  console.log("2.4. (x)"); /* 7 != 0    */
+  console.log("2.5. (x)"); /* 7 !== '7' */
   console.log("2.6. ( )"); /* 7 != '7'  */
   console.log("2.7. ( )"); /* 7 != 7    */
 }
@@ -35,25 +35,25 @@ function question3() {
   // Question 3:
   // Which of the following expressions are truthy?
 
-  console.log("3.1. ( )"); /* !0         */
+  console.log("3.1. (x)"); /* !0         */
   console.log("3.2. ( )"); /* !1         */
-  console.log("3.3. ( )"); /* -1         */
+  console.log("3.3. (x)"); /* -1         */
   console.log("3.4. ( )"); /* !"hello!"  */
   console.log("3.5. ( )"); /* null       */
-  console.log("3.6. ( )"); /* !undefined */
-  console.log("3.7. ( )"); /* !NaN       */
+  console.log("3.6. (x)"); /* !undefined */
+  console.log("3.7. (x)"); /* !NaN       */
 }
 
 function question4() {
   // Question 4:
   // Which of the following are valid objects?
 
-  console.log("4.1. ( )"); /* {}                                       */
+  console.log("4.1. (x)"); /* {}                                       */
   console.log("4.2. ( )"); /* { 'hello' }                              */
-  console.log("4.3. ( )"); /* { name: 'I am fruit' }                   */
-  console.log("4.4. ( )"); /* {'brand-name': 'Dior' }                  */
+  console.log("4.3. (x)"); /* { name: 'I am fruit' }                   */
+  console.log("4.4. (x)"); /* {'brand-name': 'Dior' }                  */
   console.log("4.5. ( )"); /* { brand-name: 'Channel' }                */
-  console.log("4.6. ( )"); /* { cool_name: 'bob' + ' Dylan', age: 25 } */
+  console.log("4.6. (x)"); /* { cool_name: 'bob' + ' Dylan', age: 25 } */
 }
 
 function question5() {
@@ -62,19 +62,19 @@ function question5() {
   // and the type of each element in the provided array.
 
   // 5.1. ['cat', 42, 'bird']
-  console.log("5.1. Number of elements ( )", ["type", "type", "type"]);
+  console.log("5.1. Number of elements (3)", ["string", "number", "string"]);
 
   // 5.2. [[], 24, 'hello', true]
-  console.log("5.2. Number of elements ( )", []);
+  console.log("5.2. Number of elements (4)", ["object(array)", "number", "string", "boolean"]);
 
   // 5.3. []
-  console.log("5.3. Number of elements ( )", []);
+  console.log("5.3. Number of elements (0)", []);
 
   // 5.4. [['romeo', 'juliet'], false]
-  console.log("5.4. Number of elements ( )", []);
+  console.log("5.4. Number of elements (2)", ["object(array)", "boolean"]);
 
   // 5.5. [{name: 'bob', age: 23}, {name: 'paul', age: 33}]
-  console.log("5.5. Number of elements ( )", []);
+  console.log("5.5. Number of elements (2)", ["object", "object"]);
 }
 
 function question6() {
@@ -83,16 +83,16 @@ function question6() {
   // e.g. console.log("6.1.", <TYPE>, <VALUE>)
 
   // 6.1. let name = 'bob';
-  console.log("6.1.", "type", "value");
+  console.log("6.1.", "string", "bob");
 
   // 6.2. let age = 45;
-  console.log("6.2.");
+  console.log("6.2.", "number", "45");
 
   // 6.3. let isMarried = false;
-  console.log("6.3.");
+  console.log("6.3.", "boolean", "false");
 
   // 6.4. let kids = [{name: 'Morty', age: 3}, {name: 'Summer', age: 7}]
-  console.log("6.4.");
+  console.log("6.4.", "object(array)", "{name: 'Morty', age: 3}, {name: 'Summer', age: 7}");
 }
 
 // Once you are done type the following in the terminal to test your answer(s).
