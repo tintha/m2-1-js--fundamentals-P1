cce356ae87e236d92f038c90471fd28e1c05451d
Thira Inthavongsa <tintha@gmail.com>
2020-09-27 15:55:18 -0400

    Exercise 11.. not quite right yet

--- __workshop/exercise-11.js
+++ __workshop/exercise-11.js
@@ -15,6 +15,20 @@ function exercise11() {
   //
   // Write code between the lines (below)
   // -----------------------------------------------------------------
+  let board = "";
+
+  for (let i = 1; i <= 8; i++) {
+    for (let j = 1; j <= 8; j++) {
+      if ((j % 2) == (i % 2)) {
+        board += "#";
+      } else {
+        board += "_";
+      }
+    }
+    board += "\n";
+  }
+
+  console.log(board);
   // -----------------------------------------------------------------
   // Write code between the lines (above)
   //
