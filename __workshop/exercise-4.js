469ac03189e7758a49d4729ad812e5bfc624c0db
Thira Inthavongsa <tintha@gmail.com>
2020-09-26 23:02:19 -0400

    Finished ex 4

--- __workshop/exercise-4.js
+++ __workshop/exercise-4.js
@@ -9,8 +9,8 @@ function exercise4() {
   //
   // Edit only the code between the lines (below)
   // -----------------------------------------------------------------
-  for (let number = 0; number < 12; number++) {
-    console.log(square);
+  for (let number = 0; number <= 12; number++) {
+    console.log(number * number);
   }
   // -----------------------------------------------------------------
   // Edit only the code between the lines (above)
